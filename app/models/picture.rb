b1f346e582f6c76835ba5e4a0c9f6ad9454ef1c9
kan.akutsu <sakamotoshizuya@gmail.com>
2014-11-01 13:05:08 +0900

    change views

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,7 @@
 class Picture < ActiveRecord::Base
 
   #photoをattachファイルとする。stylesで画像サイズを定義する
-  has_attached_file :photo, :styles => { medium: "300x300>", thumb: "100x100#" }
+  has_attached_file :photo, :styles => { medium: "300x300>" }
 
   #ファイルの拡張子を指定
   validates_attachment :photo, :content_type  => { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
