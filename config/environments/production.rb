71a3fbde22666ee43797cd4f263ab5cba504c9e1
kan.akutsu <sakamotoshizuya@gmail.com>
2014-11-01 12:25:22 +0900

    Update prodection.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ UploadTest::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # changed 11/1
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
